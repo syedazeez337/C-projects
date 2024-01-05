d801c31da5772da869ba16d50bc41372f672c1b7
syedazeez337 <67051473+syedazeez337@users.noreply.github.com>
2024-01-05 15:25:08 +0530

    Create tipcalcimproved.c

---
+++ FunProject01/tipcalcimproved.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+				double bill, percent;
+				int numofpeople;
+
+				bool quit = false;
+				while(!quit)
+				{
+                char choice;
+                printf("Enter your choice calculate tip(t) or quit(q): ");
+                choice = getchar();
+                //printf("%c %X\n", choice, choice);
+                /*
+                This bug in this programme: the getchar() takes the return(enter)
+                from the keyboard as the input to the next while loop
+                */
+								if(choice == 'q')
+								{
+												quit = true;
+												printf("Thank you! Bye...\n");
+												break;
+								}
+								else
+								{
+												printf("Enter the bill: ");
+												scanf("%lf", &bill);
+												printf("Enter tip percent: ");
+												scanf("%lf", &percent);
+												printf("Enter number of people: ");
+												scanf("%d", &numofpeople);
+
+												double tipamt = bill * (percent / 100);
+												double total = bill + tipamt;
+												double eachperson = total / numofpeople;
+
+												printf("Tip amount: %.2lf\n", tipamt);
+												printf("Total bill: %.2lf\n", total);
+												printf("Shared cost: %.2lf\n", eachperson);
+								}
+				}
+}
